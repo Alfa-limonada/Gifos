6eff2d6bad8627395340437237444c39c670b788
Alfa Limonada <30637998+Alfa-limonada@users.noreply.github.com>
2020-06-05 20:41:54 -0500

    seprar funciones capture 1

--- app/capture.js
+++ app/capture.js
@@ -1,70 +1,63 @@
-function getStream() {
+let instance = null;
+let myGifOs = [];
+let start = document.getElementById("btnCapturar");
+let stop = document.getElementById("btnListo");
+stop.addEventListener("click", stopStream);
+start.addEventListener("click", startStream);
+
+// singleton nos permite tener una instancia unica de una clase
+function getInstanceMediaRecorder(mediaStreamObj) {
+  if (instance === null) {
+    instance = new MediaRecorder(mediaStreamObj);
+    return instance;
+  } else {
+    return instance;
+  }
+}
+// método para obtener el stream del usuario
+async function getStream() {
   let constraintObj = {
     audio: false,
     video: {
       facingMode: "user",
-      height: { max: 480 }
-    }
+      height: { max: 480 },
+    },
   };
-
-  //método para obtener el stream del usuario
-  navigator.mediaDevices
-    .getUserMedia(constraintObj)
-    //promesa que captura el media stream
-    .then(function(mediaStreamObj) {
-      //Conecta el mediaStreamObj al src de  video Stream
-      let videoStream = document.getElementById("videoStream");
-      if ("srcObject" in videoStream) {
-        videoStream.srcObject = mediaStreamObj;
-      } else {
-        videoStream.src = window.URL.createObjectURL(mediaStreamObj);
-      }
-      //Muestra el video en cuanto lo puede hacer.
-      videoStream.onloadedmetadata = function(ev) {
-        videoStream.play();
-      };
-
-      //add listeners for saving video/audio
-      let start = document.getElementById("btnCapturar");
-      let stop = document.getElementById("btnListo");
-      let videoSave = document.getElementById("videoSave");
-      let mediaRecorder = new MediaRecorder(mediaStreamObj);
-      let myGifOs = [];
-
-      //comienza a grabar
-      start.addEventListener("click", ev => {
-        //comienza a grabar
-
-        mediaRecorder.start();
-        console.log(mediaRecorder.state);
-      });
-
-      //Deja de grabar
-      stop.addEventListener("click", ev => {
-        mediaRecorder.stop();
-        console.log(mediaRecorder.state);
-        //apaga la cámara
-        // mediaStreamObj.getTracks()[0].stop(); 
-      });
-
-      //Guardar en array mientas hay data disponible:
-      mediaRecorder.ondataavailable = function(ev) {
-        myGifOs.push(ev.data);
-      };
-
-      //Cuando para guardar en blob
-      mediaRecorder.onstop = ev => {
-        let blob = new Blob(myGifOs, { type: "video/mp4;" });
-        // chunks = []; Si uno quisiera blanquear el array
-        let videoURL = window.URL.createObjectURL(blob);
-        videoSave.src = videoURL;
-      };
-    })
-    //Si no funca
-    .catch(function(err) {
-      console.log(err.name, err.message);
-      alert("Tu video no puede ser capturado");
-    });
+  try {
+    const mediaStreamObj = await navigator.mediaDevices.getUserMedia(
+      constraintObj
+    );
+    let videoStream = document.getElementById("videoStream");
+    //Conecta el mediaStreamObj al src de  video Stream
+    if ("srcObject" in videoStream) {
+      videoStream.srcObject = mediaStreamObj;
+    } else {
+      videoStream.src = window.URL.createObjectURL(mediaStreamObj);
+    }
+    //Muestra el video en cuanto lo puede hacer.
+    videoStream.onloadedmetadata = videoStream.play();
+    return mediaStreamObj;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-getStream();
+//Empieza a grabar 
+
+async function startStream() {
+  const mediaStreamObj = await getStream();
+  let mediaRecorder = getInstanceMediaRecorder(mediaStreamObj);
+  mediaRecorder.start();
+  console.log(mediaRecorder.state);  
+}
+
+//Para de grabar
+async function stopStream() {
+  const mediaStreamObj = await getStream();
+  let mediaRecorder = getInstanceMediaRecorder(mediaStreamObj);
+  mediaRecorder.stop();
+  console.log(mediaRecorder.state);
+  mediaRecorder.ondataavailable = function (ev) {
+    myGifOs.push(ev.data);
+  };
+}
--- app/search.js
+++ app/search.js
@@ -12,12 +12,26 @@ document.getElementById("keyword").addEventListener("keydown", function(ev) {
 async function searchKeyword() {
   let keyword = document.getElementById("keyword").value.trim(); //toma el keyword del imput
   let searchUrl = `${GYPHY_BASE_URL}search?api_key=${APIKEY}&q=${keyword}&limit=4`; //endpoint para search
+  
+ 
   let searchWrap = document.querySelector("#searchWrap");
   //valida que no esté  vacío el campo de búsqueda
   if (keyword === "") {
     return alert("Por favor ingresa una búsqueda");
   }
-  await fetchApi(searchUrl); // consumo el api, espero la respuesta (gracias al await) y la retorno.
+  await fetchApi(searchUrl); // consumo el api, espero la respuesta (gracias al await) y la retorno.  
   append(content, searchWrap); // pongo resultados en DOM
+  appendTags();
   document.querySelector("#keyword").value = ""; //clear form
+};
+
+async function appendTags() {
+  let keyword = document.getElementById("keyword").value.trim(); //toma el keyword del imput
+  let tagsUrl = `http://api.giphy.com/v1/tags/related/${keyword}?api_key=${APIKEY}&limit=4`;
+  console.log(tagsUrl);
+  await fetchApi(tagsUrl); // consumo api de tags
+  console.log(content) 
+
+  //Acá hacer elappend a los 3 cositos con sugerencias de búsqueda.
+  
 }
